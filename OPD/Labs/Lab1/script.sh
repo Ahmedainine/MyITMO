ff62e470fffba058c33fe57bac5a5ab09c3f7454
ahmedndb21005 <ahmedndb21005@gmail.com>
2025-09-19 14:47:40 +0300

    Upload My Folder and Files

---
+++ OPD/Labs/Lab1/script.sh
@@ -0,0 +1,274 @@
+#Question1 : 
+
+mkdir lab0
+cd lab0
+mkdir -p braviary4/machamp/chimecho
+mkdir braviary4/machamp/mamoswine
+mkdir -p braviary4/swanna/sandshrew
+mkdir braviary4/swanna/parasect
+mkdir braviary4/swanna/shelmet
+mkdir braviary4/swanna/squirtle
+touch braviary4/kabutops
+touch braviary4/hippowdon
+touch braviary4/fraxure
+touch braviary4/machamp/petilil
+touch braviary4/machamp/porygon
+touch braviary4/machamp/quilava
+touch braviary4/swanna/slakoth
+touch braviary4/swanna/fearow
+touch charizard7
+touch cottonee3
+touch rhydon4
+mkdir -p finneon8/walrein/hypno
+mkdir finneon8/walrein/electrode
+mkdir finneon8/walrein/dragonite
+mkdir -p finneon8/pupitar/mienfoo
+mkdir finneon8/pupitar/budew
+mkdir -p finneon8/ekans/bisharp
+mkdir finneon8/ekans/houndoom
+mkdir finneon8/ekans/milotic
+touch finneon8/seadra
+touch finneon8/oddish
+touch finneon8/reuniclus
+touch finneon8/walrein/tentacruel
+touch finneon8/walrein/skuntank
+touch finneon8/walrein/ekans
+touch finneon8/pupitar/glalie
+touch finneon8/pupitar/purrloin
+touch finneon8/pupitar/zweilous
+touch finneon8/ekans/axew
+touch finneon8/ekans/jumpluff
+mkdir -p hoppip4/omastar/fraxure
+mkdir -p hoppip4/gastrodon/swellow
+mkdir -p hoppip4/koffing/sunkern
+mkdir hoppip4/koffing/chingling
+mkdir hoppip4/koffing/sandile
+touch hoppip4/ninjask
+touch hoppip4/omastar/krokorok
+touch hoppip4/gastrodon/skiploom
+touch hoppip4/gastrodon/swinub
+touch hoppip4/gastrodon/leavanny
+touch hoppip4/koffing/duosion
+touch hoppip4/koffing/solosis
+
+
+echo -e 'Способности Mountain Peak Torrent Battle Armor Swift\nSwim' > braviary4/kabutops
+echo -e 'Ходы After You Covet Giga Drain Heal Bell Helping Hand\nSeed bomb Sleep talk Snore Synthesis Worry Seed' > braviary4/machamp/petilil
+echo -e 'Способности\nLast Chance Trace Download' > braviary4/machamp/porygon
+echo -e 'Живет Cave\nMountain' > braviary4/machamp/quilava
+echo 'Развитые Способности Forewarn' >braviary4/swanna/slakoth
+echo -e 'Тип диеты\nomnivore' > braviary4/swanna/fearow
+echo 'Тип покемона GROUND NONE'> braviary4/hippowdon
+echo -e 'Развитые\nСпособности Unnerve' > braviary4/fraxure
+echo -e 'weight=199.5 height=67.0 atk=8\ndef=8' > charizard7
+echo -e 'weight=1.3 height=12.0 atk=3\ndef=6' > cottonee3
+echo -e 'Возможности Overland=4 Surface=10 Underwater=10\nJump3=0 Power=4 Intelligence=4 Gilled=0 Reach=0' > finneon8/walrein/tentacruel
+echo -e 'Возможности\nOverland=8 Surface=6 Jump=3 Power=2 Intelligence=4' > finneon8/walrein/skuntank
+echo -e 'Развитые\nСпособности Unnerve' > finneon8/walrein/ekans
+echo -e 'weight=565.5 height=59.0 atk=8\ndef=8' > finneon8/pupitar/glalie
+echo -e 'weight=22.3 height=16.0 atk=5\ndef=4' > finneon8/pupitar/purrloin
+echo -e 'Способности Focus Energy Bite Headbutt Dragonbreath\nRoar Crunch Slam Dragon Pulse Work Up Dragon Rush Body Slam Scary Face\nHyper Voice Outrage' > finneon8/pupitar/zweilous
+echo  'Тип покемона WATER NONE' > finneon8/seadra
+echo -e 'Живет Cave\nMountain' > finneon8/ekans/axew
+echo  'satk=6 sdef=9 spd=11'  > finneon8/ekans/jumpluff
+echo -e 'satk=8 sdef=7\nspd=3' > finneon8/oddish
+echo 'Тип покемона PSYCHIC NONE' > finneon8/reuniclus
+echo -e 'Тип диеты\nCarnivore' > hoppip4/omastar/krokorok
+echo -e 'Возможности Overland=6 Surface=2 Sky=8 Jump=6\nPower1=0 Intelligence=4 Sprouter=0' > hoppip4/gastrodon/skiploom
+echo -e 'stak=3 sdef=3\nspd=5' > hoppip4/gastrodon/swinub
+echo -e 'weight=45.2 height=47.0 atk=10\ndef=8' > hoppip4/gastrodon/leavanny 
+echo -e 'Способности Mind Mold Overcoat Magic\nGuard' > hoppip4/koffing/duosion
+echo -e 'Способности Psywave Reflect Rollout Snatch Hidden\nPower Light Screen Charm Recover Psyshock Endeavor Future Sight pain\nSplit Psychic Skill Swap Heal Block Wonder Room' > hoppip4/koffing/solosis
+echo -e 'Развитые\nСпособности Infiltrator' > hoppip4/ninjask
+echo -e 'Ходы Ancientpower Aqua Tail\nAvanlanche Block Body Slam Counter Double-Edge Dragon Pulse Drill Run\nDynamicpunch Earth Power Endeavor Fire Punch Focus Punch Fury Cutter\nIce Punch Icy Wind Iron Tail Mega Kick Mega Punch Mud-Slap Outrage\nRollout Shock Wave Seismic Toss Sleep Talk Snore spite Stealth Rock\nSuperpower Thunderpunch Uproar' > rhydon4
+
+
+#Question2 :
+
+
+chmod 737 braviary4
+chmod 060 braviary4/kabutops
+chmod 315 braviary4/machamp
+chmod 753 braviary4/machamp/chimecho
+chmod 400 braviary4/machamp/petilil
+chmod 524 braviary4/machamp/mamoswine
+chmod 044 braviary4/machamp/porygon
+chmod 066 braviary4/machamp/quilava
+chmod 363 braviary4/swanna
+chmod 737 braviary4/swanna/sandshrew
+chmod 737 braviary4/swanna/parasect
+chmod 062 braviary4/swanna/slakoth
+chmod 006 braviary4/swanna/fearow
+chmod 753 braviary4/swanna/shelmet
+chmod 312 braviary4/swanna/squirtle
+chmod 600 braviary4/hippowdon
+chmod 044 braviary4/fraxure
+chmod 604 charizard7
+chmod 064 cottonee3
+chmod 777 finneon8
+chmod 537 finneon8/walrein
+chmod 305 finneon8/walrein/hypno
+chmod 044 finneon8/walrein/tentacruel
+chmod 307 finneon8/walrein/electrode
+chmod 624 finneon8/walrein/skuntank
+chmod 600 finneon8/walrein/ekans
+chmod 513 finneon8/walrein/dragonite
+chmod 363 finneon8/pupitar
+chmod 315 finneon8/pupitar/mienfoo
+chmod 312 finneon8/pupitar/budew
+chmod 440 finneon8/pupitar/glalie
+chmod 444 finneon8/pupitar/purrloin
+chmod 620 finneon8/pupitar/zweilous
+chmod 440 finneon8/seadra
+chmod 357 finneon8/ekans
+chmod 770 finneon8/ekans/bisharp
+chmod 577 finneon8/ekans/houndoom
+chmod 770 finneon8/ekans/milotic
+chmod 404 finneon8/ekans/axew
+chmod 444 finneon8/ekans/jumpluff
+chmod 060 finneon8/oddish
+chmod 604 finneon8/reuniclus
+chmod 333 hoppip4
+chmod 764 hoppip4/omastar
+chmod 604 hoppip4/omastar/krokorok
+chmod 361 hoppip4/omastar/fraxure
+chmod 576 hoppip4/gastrodon
+chmod 400 hoppip4/gastrodon/skiploom
+chmod 444 hoppip4/gastrodon/swinub
+chmod 770 hoppip4/gastrodon/swellow
+chmod 622 hoppip4/gastrodon/leavanny
+chmod 555 hoppip4/koffing
+chmod 576 hoppip4/koffing/sunkern
+chmod 373 hoppip4/koffing/chingling
+chmod 711 hoppip4/koffing/sandile
+chmod 440 hoppip4/koffing/duosion
+chmod 664 hoppip4/koffing/solosis
+chmod 004 hoppip4/ninjask
+chmod 644 rhydon4
+
+
+# Question3  :
+
+
+#3.1
+cat braviary4/fraxure finneon8/walrein/ekans finneon8/pupitar/purrloin > rhydon4_66
+
+#3.2
+cat charizard7 > finneon8/walrein/ekanscharizard
+
+#3.3
+cp -r finneon8 hoppip4/koffing/sandile
+
+#3.4
+ln -s ../../rhydon4 braviary4/swanna/slakothrhydon
+
+#3.5
+cp rhydon4 braviary4/swanna/squirtle/
+
+#3.6
+
+#Проблема #1
+#скопировать рекурсивно директорию hoppip4 в директорию hoppip4/koffing/sandilel
+#$ cp -r hoppip4 hoppip4/koffing/sandile
+#Данная команда приведет к ошибке
+#cp: cannot copy a directory, 'hoppip4', into itself, 'hoppip4/koffing/sandilel'
+#Проблема заключается в том, что мы пытаемся рекурсивно копировать директорию в саму себя, что приводит к бесконечному циклу
+
+mkdir -p temp/hoppip4
+cp -r hoppip4/* temp/hoppip4
+mv temp/hoppip4 hoppip4/koffing/sandile
+rmdir temp
+
+
+
+
+
+
+
+#3.7
+ln -s hoppip4 Copy_11
+
+#3.8
+ln -s ../../charizard7 finneon8/pupitar/glaliecharizard
+
+#3.9
+ln cottonee3 finneon8/walrein/skuntankcottonee
+
+#3.10
+cat charizard7 > finneon8/ekans/axewcharizard
+
+#3.11
+ln -s braviary4 Copy_31
+
+#3.12
+cat finneon8/walrein/skuntank hoppip4/ninjask braviary4/swanna/fearow > rhydon4_87
+
+#3.13
+cp charizard7 finneon8/ekans/houndoom/
+
+#3.14
+ln cottonee3 braviary4/machamp/quilavacottonee
+
+
+
+
+#Question 4 :
+
+
+#4.1
+wc -m cottonee3 2>/dev/null > cottonee3
+
+#4.2
+ls hoppip4 2>/dev/null | sort
+
+#4.3
+cat -n braviary4/hippowdon braviary4/fraxure finneon8/walrein/tentacruel finneon8/walrein/skuntank finneon8/walrein/ekans finneon8/pupitar/glalie finneon8/pupitar/purrloin finneon8/pupitar/zweilous finneon8/seadra finneon8/ekans/axew 2>/dev/null | grep -vi "re"
+
+
+
+#4.4
+ls -lR lab0 2>&1 | grep '^p' | sort -k5 -nr | head -n 2
+
+
+#4.5
+ls -lR lab0 2>&1 | sort -k2 -nr | tail -n 4
+
+
+#4.6
+wc -l lab0/*/*h 2>/dev/null | sort -n
+
+
+#4.7
+ls -lR lab0 2>/dev/null | grep "in" | sort -rk6,7 | head -n4
+
+
+
+#4.8
+cat -n braviary4/*/o* braviary4/o* finneon8/*/o* finneon8/o* hoppip4/*/o* hoppip4/o* 2>&1 | sort -r
+
+
+#4.9
+wc -m lab0/*/*a 2>&1 | sort -rn
+
+#4.10
+ls -lRt lab0 | sort -rk 6,7 | tail -n 2
+
+
+#4.11
+ls -lR lab0 2>/tmp/errors.txt | sort -k2,2n | head -n 2
+
+#4.12
+wc -m braviary4/* 2>/dev/null | sort -n
+
+
+#Question5:
+
+
+rm rhydon4
+rm lab0/finneon8/oddish
+rm Copy_*
+rm lab0/finneon8/walrein/skuntankcotton*
+rmdir hoppip4
+rmdir lab0/hoppip4/koffing/sandile
+
+
